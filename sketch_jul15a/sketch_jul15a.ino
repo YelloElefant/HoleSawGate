dde0b6a9207ad394d8bf60d7ca05dc008ef35647
YelloElefant <alextrotternz@gmail.com>
2022-08-03 21:56:30 +1200

    mostly done prity much finished

--- sketch_jul15a/sketch_jul15a.ino
+++ sketch_jul15a/sketch_jul15a.ino
@@ -1,16 +1,50 @@
 
+
+
+int input;
+
+
+
 void setup() {
-  // initialize digital pin LED_BUILTIN as an output.
+  Serial.begin(9600);
   pinMode(A4, OUTPUT);
+  pinMode(A3, OUTPUT);
+  pinMode(A1, OUTPUT);
+  pinMode(A5, INPUT);
+  in();
+  
+  Serial.print("start");
+  digitalWrite(A4, 1);
+  digitalWrite(A1, 1);
 }
 
-
 void loop() {
-
   int del = random(1000,5000);
   
-  digitalWrite(A4, HIGH);   
-  delay(del);                
-  digitalWrite(A4, LOW);  
-  delay(del);                
+  Serial.print(input);
+   
+  
+  if (input == false){
+    delay(del);
+    digitalWrite(A3, 1);
+    digitalWrite(A1, 0);
+    digitalWrite(A4, 0);
+    delay(3000);
+    digitalWrite(A4, 1);
+    digitalWrite(A3, 0);
+    digitalWrite(A1, 1);
+    //delay(1000);
+    in();
+  }
+  else {
+    in();
+  }
+
+
+  
+}
+
+void in(){
+  input = digitalRead(A5); 
+  Serial.print(input);
 }
